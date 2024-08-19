a0df1aae00822b7956a701216f6880d4e1148c12
Bruce Lam <xiaohuilam@users.noreply.github.com>
2024-08-19 23:29:36 +0800

    Merge https://github.com/projectclone/epusdt/commit/30dbcd86d66e55f76f3c27493d8e01f7a525d7b6

--- src/model/service/order_service.go
+++ src/model/service/order_service.go
@@ -28,7 +28,7 @@ import (
 const (
 	CnyMinimumPaymentAmount  = 0.01 // cny最低支付金额
 	UsdtMinimumPaymentAmount = 0.01 // usdt最低支付金额
-	UsdtAmountPerIncrement   = 0.01 // usdt每次递增金额
+	UsdtAmountPerIncrement   = 0.0001 // usdt每次递增金额
 	IncrementalMaximumNumber = 100  // 最大递增次数
 )
 
